45503ca5ea2f934bb981f0a52ab1439ba51fd075
Miroslav Wise <mipik1997@gmail.com>
2026-05-15 18:23:02 +0300

    --fix update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,9 @@
-import "~/App.css"
 import Chat from "./features/chat"
 import ListChat from "./features/list-chat"
 
 function App() {
   return (
-    <main className="w-full grid grid-cols-[10rem_minmax(0,1fr)] h-full min-h-0">
+    <main className="w-full h-screen overflow-hidden grid grid-cols-[10rem_minmax(0,1fr)] min-h-0">
       <ListChat />
       <Chat />
     </main>
